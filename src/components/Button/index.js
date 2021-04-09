8bf18d9088bc124f36c0988ce50435b31e5d4531
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-08 22:26:48 -0300

    added style with Styled-Component from Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,24 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+import { ButtonContainer, TextButton } from './styles';
 
 export const Button = ({ children }) => (
-  <TouchableOpacity style={styles.button}>
-    <Text style={styles.buttonText}>{String(children).toUpperCase()}</Text>
-  </TouchableOpacity>
+  <ButtonContainer>
+    <TextButton>{String(children).toUpperCase()}</TextButton>
+  </ButtonContainer>
 );
-
-const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'blue',
-    width: 327,
-    height: 56,
-    borderRadius: 36,
-  },
-  buttonText: {
-    fontSize: 16,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-});
