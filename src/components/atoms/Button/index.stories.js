30b26da9e410a4fc9536b1412a13059edb4f0a74
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-08 22:57:35 -0300

    updated Button story

--- src/components/atoms/Button/index.stories.js
+++ src/components/atoms/Button/index.stories.js
@@ -1,10 +1,16 @@
 import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
+import { boolean, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 
 import { Button } from './index';
+import { Wrapper } from '../../wrapper';
 
-storiesOf('Button', module).add('Standard', () => (
-  <Button>{text('TextButton', 'Your Text in Button')}</Button>
-));
+storiesOf('Button', module)
+  .addDecorator(getStory => <Wrapper>{getStory()}</Wrapper>)
+  .add('Standard', () => {
+    const value = text('Text to the Button', 'Your Text in Button');
+    const outline = boolean('Outline', false);
+
+    return <Button outline={outline}>{value}</Button>;
+  });
