6229013ff1bf290e88f3115039a9d178e3a252ed
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-08 23:58:38 -0300

    added Text styles

---
+++ src/components/atoms/Text/index.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+
+const BaseText = styled.Text`
+  margin-top: ${({ theme, mt }) => theme.metrics.px(mt || 0)}px;
+  margin-right: ${({ theme, mt }) => theme.metrics.px(mt || 0)}px;
+  margin-bottom: ${({ theme, mb }) => theme.metrics.px(mb || 0)}px;
+  margin-left: ${({ theme, ml }) => theme.metrics.px(ml || 0)}px;
+`;
+
+export const Title = styled(BaseText)`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${({ theme }) => theme.metrics.px(28)}px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const CardTitle = styled(BaseText)`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${({ theme }) => theme.metrics.px(16)}px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const LinkText = styled(BaseText)`
+  font-family: ${({ theme }) => theme.fonts.semiBold};
+  font-size: ${({ theme }) => theme.metrics.px(16)}px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const RegularText = styled(BaseText)`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${({ theme }) => theme.metrics.px(14)}px;
+  color: ${({ theme }) => theme.colors.dark};
+`;
+
+export const InputLabel = styled(BaseText)`
+  font-family: ${({ theme }) => theme.fonts.semiBold};
+  font-size: ${({ theme }) => theme.metrics.px(14)}px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
