2f159291d5daca388aff9bf706db9f8d491e357a
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-10 20:39:52 -0300

    added Routes in App

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { theme } from './styles/theme';
-import Storybook from '../storybook';
+import { Routes } from './routes';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Storybook />
+      <Routes />
     </ThemeProvider>
   );
 };
