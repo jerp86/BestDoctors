97d3c3a2f32fd7b85dc0104e7b6e9b7c6d09af11
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-10 20:39:26 -0300

    added AuthNavigator in routes

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AuthNavigator } from '~/modules/Auth/routes';
+
+export const Routes = () => {
+  return (
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
+  );
+};
