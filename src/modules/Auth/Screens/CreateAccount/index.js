d875c78a50e14a62496e1bd6bf0bdf73565842f2
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-21 15:24:18 -0300

    fix CreateAccount: insert navigate to SelectAccountType

--- src/modules/Auth/Screens/CreateAccount/index.js
+++ src/modules/Auth/Screens/CreateAccount/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import { AuthWrapper, Button, Input } from '~/components';
 
-export const CreateAccountScreen = () => {
+export const CreateAccountScreen = ({ navigation }) => {
+  const goToSelectAccountType = () => {
+    navigation.navigate('SelectAccountType');
+  };
+
   return (
     <AuthWrapper
       title="Crie sua conta"
-      bottomChildren={<Button>PRÓXIMO</Button>}>
+      bottomChildren={<Button onPress={goToSelectAccountType}>PRÓXIMO</Button>}>
       <Input label="Nome" placeholder="Insira seu nome completo" />
       <Input label="E-mail" placeholder="Insira seu melhor e-mail" mt={24} />
       <Input label="Celular" placeholder="(00) 0 0000-0000" mt={24} />
