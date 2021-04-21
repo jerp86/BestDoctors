fd3cf315756abcb31c3aa5fb25118ac8752ed17c
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-21 14:54:59 -0300

    fix TextLink: update BaseContainer to BaseTouchContainer

--- src/components/atoms/TextLink/index.js
+++ src/components/atoms/TextLink/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BaseContainer } from '../Container';
+import { BaseTouchContainer } from '../Container';
 import { LinkText } from '../Text';
 
 export const TextLink = ({ children, ...rest }) => (
-  <BaseContainer h={24} align="flex-end" justify="flex-end" {...rest}>
+  <BaseTouchContainer h={24} align="flex-end" justify="flex-end" {...rest}>
     <LinkText>{children}</LinkText>
-  </BaseContainer>
+  </BaseTouchContainer>
 );
