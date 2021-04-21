8cf4f6cf21e3c24482d047fd208d9e7400482c23
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-21 13:48:45 -0300

    added BaseContainer and BaseTouchable in atoms

--- src/components/atoms/Container/index.js
+++ src/components/atoms/Container/index.js
@@ -1,5 +1,43 @@
 import styled from 'styled-components/native';
 
+export const BaseContainer = styled.View`
+  flex-direction: ${({ dir }) => dir || 'column'};
+  align-items: ${({ align }) => align || 'flex-start'};
+  justify-content: ${({ justify }) => justify || 'flex-start'};
+
+  width: ${({ theme, w }) => (w ? `${theme.metrics.px(w)}px` : '100%')};
+  height: ${({ theme, h }) => (h ? `${theme.metrics.px(h)}px` : '100%')};
+
+  margin-top: ${({ theme, mt }) => theme.metrics.px(mt || 0)}px;
+  margin-right: ${({ theme, mr }) => theme.metrics.px(mr || 0)}px;
+  margin-bottom: ${({ theme, mb }) => theme.metrics.px(mb || 0)}px;
+  margin-left: ${({ theme, ml }) => theme.metrics.px(ml || 0)}px;
+
+  padding-top: ${({ theme, pt }) => theme.metrics.px(pt || 0)}px;
+  padding-right: ${({ theme, pr }) => theme.metrics.px(pr || 0)}px;
+  padding-bottom: ${({ theme, pb }) => theme.metrics.px(pb || 0)}px;
+  padding-left: ${({ theme, pl }) => theme.metrics.px(pl || 0)}px;
+`;
+
+export const BaseTouchContainer = styled.TouchableOpacity`
+  flex-direction: ${({ dir }) => dir || 'column'};
+  align-items: ${({ align }) => align || 'flex-start'};
+  justify-content: ${({ justify }) => justify || 'flex-start'};
+
+  width: ${({ theme, w }) => (w ? `${theme.metrics.px(w)}px` : '100%')};
+  height: ${({ theme, h }) => (h ? `${theme.metrics.px(h)}px` : '100%')};
+
+  margin-top: ${({ theme, mt }) => theme.metrics.px(mt || 0)}px;
+  margin-right: ${({ theme, mr }) => theme.metrics.px(mr || 0)}px;
+  margin-bottom: ${({ theme, mb }) => theme.metrics.px(mb || 0)}px;
+  margin-left: ${({ theme, ml }) => theme.metrics.px(ml || 0)}px;
+
+  padding-top: ${({ theme, pt }) => theme.metrics.px(pt || 0)}px;
+  padding-right: ${({ theme, pr }) => theme.metrics.px(pr || 0)}px;
+  padding-bottom: ${({ theme, pb }) => theme.metrics.px(pb || 0)}px;
+  padding-left: ${({ theme, pl }) => theme.metrics.px(pl || 0)}px;
+`;
+
 export const Container = styled.View`
   flex: 1;
   align-items: center;
