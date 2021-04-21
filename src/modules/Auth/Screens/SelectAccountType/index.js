ddcb29cadcb5ea1066bbd8490155dd4367910272
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-21 16:15:44 -0300

    updated state in SelectAccountType and inserted navigate

--- src/modules/Auth/Screens/SelectAccountType/index.js
+++ src/modules/Auth/Screens/SelectAccountType/index.js
@@ -1,30 +1,40 @@
 import React from 'react';
+import { useState } from 'react';
 
 import { AuthWrapper, Button, SelectTypeCard } from '~/components';
 
-export const SelectAccountTypeScreen = () => {
+export const SelectAccountTypeScreen = ({ navigation }) => {
+  const [accountType, setAccountType] = useState();
+
+  const changeAccountType = type => setAccountType(type);
+
+  const goToConfirmPhone = () => navigation.navigate('ConfirmPhone');
+
   return (
     <AuthWrapper
       title="Escolha o tipo de conta"
-      bottomChildren={<Button>PRÓXIMO</Button>}>
+      bottomChildren={<Button onPress={goToConfirmPhone}>PRÓXIMO</Button>}>
       <SelectTypeCard
         title="PACIENTE"
         description="Vou buscar as clínicas e médicos próximos a mim para agendar consultas e ter um histórico."
-        selected={false}
+        selected={accountType === 'patient'}
         mb={24}
+        onPress={() => changeAccountType('patient')}
       />
 
       <SelectTypeCard
         title="MÉDICO"
         description="Irei cadastrar meu perfil médico para anteder meus pacientes e me associar à uma clinica (opcional)."
-        selected={false}
+        selected={accountType === 'doctor'}
         mb={24}
+        onPress={() => changeAccountType('doctor')}
       />
 
       <SelectTypeCard
         title="CLÍNICA MÉDICA"
         description="Irei cadastrar minha clinica e os colaboradores médicos para atender meus clientes."
-        selected={false}
+        selected={accountType === 'clinic'}
+        onPress={() => changeAccountType('clinic')}
       />
     </AuthWrapper>
   );
