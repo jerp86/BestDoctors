b7c0d873331a80272859a204b647c833c7173329
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-21 14:57:49 -0300

    fix Login: update to AuthWrapper props

--- src/modules/Auth/Screens/Login/index.js
+++ src/modules/Auth/Screens/Login/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
 import { AuthWrapper, Button, Input, TextLink } from '~/components';
-import { ButtonContainer, ContentContainer } from './styles';
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ navigation }) => {
+  const goToCreateAccount = () => {
+    navigation.navigate('CreateAccount');
+  };
+
   return (
-    <AuthWrapper title="Bem-vindo de volta!">
-      <ContentContainer>
-        <Input label="E-mail" placeholder="Insira seu melhor e-mail..." />
-        <Input label="Senha" placeholder="Insira sua senha segura..." mt={24} />
+    <AuthWrapper
+      title="Bem-vindo de volta!"
+      bottomChildren={
+        <>
+          <Button>Entrar</Button>
+          <Button outline mt={16} onPress={goToCreateAccount}>
+            CRIAR NOVA CONTA
+          </Button>
+        </>
+      }>
+      <Input label="E-mail" placeholder="Insira seu melhor e-mail..." />
+      <Input label="Senha" placeholder="Insira sua senha segura..." mt={24} />
 
-        <TextLink mt={24}>Esqueceu sua senha?</TextLink>
-      </ContentContainer>
-
-      <ButtonContainer>
-        <Button>Entrar</Button>
-        <Button outline mt={16}>
-          CRIAR NOVA CONTA
-        </Button>
-      </ButtonContainer>
+      <TextLink mt={24}>Esqueceu sua senha?</TextLink>
     </AuthWrapper>
   );
 };
