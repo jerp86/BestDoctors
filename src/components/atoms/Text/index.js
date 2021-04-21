112d5bd473c3beeb2adee6e97aef9c39f87a0d79
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-21 13:50:10 -0300

    fix Text atoms

--- src/components/atoms/Text/index.js
+++ src/components/atoms/Text/index.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components/native';
 
 const BaseText = styled.Text`
+  width: ${({ w }) => w || 'auto'};
+
   margin-top: ${({ theme, mt }) => theme.metrics.px(mt || 0)}px;
-  margin-right: ${({ theme, mt }) => theme.metrics.px(mt || 0)}px;
+  margin-right: ${({ theme, mr }) => theme.metrics.px(mr || 0)}px;
   margin-bottom: ${({ theme, mb }) => theme.metrics.px(mb || 0)}px;
   margin-left: ${({ theme, ml }) => theme.metrics.px(ml || 0)}px;
+
+  text-align: ${({ align }) => align || 'left'};
 `;
 
 export const Title = styled(BaseText)`
@@ -31,7 +35,7 @@ export const RegularText = styled(BaseText)`
   color: ${({ theme }) => theme.colors.dark};
 `;
 
-export const InputLabel = styled.Text`
+export const InputLabel = styled(BaseText)`
   font-family: ${({ theme }) => theme.fonts.semiBold};
   font-size: ${({ theme }) => theme.metrics.px(14)}px;
   color: ${({ theme }) => theme.colors.primary};
