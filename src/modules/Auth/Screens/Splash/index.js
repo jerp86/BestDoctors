0d5f442eb9dd6e5000c6ebb0dc12b64f2b6ccccb
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-04-21 13:47:37 -0300

    fix background and added useEffect in SplashScreen

--- src/modules/Auth/Screens/Splash/index.js
+++ src/modules/Auth/Screens/Splash/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Container, Logo } from './styles';
 
-import background from '~/assets/img/auth_background.png';
+import background from '~/assets/img/background.png';
 import logoImg from '~/assets/img/logo.png';
 
-export const SplashScreen = () => {
+export const SplashScreen = ({ navigation }) => {
+  useEffect(() => {
+    setTimeout(() => navigation.navigate('Login'), 1000);
+  });
+
   return (
     <Container source={background}>
       <Logo source={logoImg} />
