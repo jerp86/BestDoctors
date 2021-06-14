5a52e6b74a25df581bf9d475842a162fbc47d20b
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-06-14 20:32:43 -0300

    added Error prop in Input molecule

--- src/components/molecules/Input/index.js
+++ src/components/molecules/Input/index.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 
 import { InputContainer, InputText } from './styles';
 
-import { InputLabel } from '../../atoms';
+import { ErrorLabel, InputLabel } from '../../atoms';
 
-export const Input = ({ label, placeholder, ...rest }) => {
+export const Input = ({ label, placeholder, error, ...rest }) => {
   const [focus, setFocus] = useState(false);
 
   return (
-    <InputContainer focus={focus} {...rest} pl={6} pt={2} pb={2}>
-      <InputLabel>{String(label).toUpperCase()}</InputLabel>
+    <>
+      <InputContainer focus={focus} {...rest} pl={6} pt={2} pb={2}>
+        <InputLabel>{String(label).toUpperCase()}</InputLabel>
 
-      <InputText
-        placeholder={placeholder}
-        onFocus={() => setFocus(true)}
-        onEndEditing={() => setFocus(false)}
-      />
-    </InputContainer>
+        <InputText
+          placeholder={placeholder}
+          onFocus={() => setFocus(true)}
+          onEndEditing={() => setFocus(false)}
+        />
+      </InputContainer>
+      {error && <ErrorLabel>{error}</ErrorLabel>}
+    </>
   );
 };
